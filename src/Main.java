1a2068b3b801975149adcf7602b0850e1028059c
Bartek Szkola <bartek326@icloud.com>
2026-03-24 22:10:26 +0100

    basic user interface

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,11 +18,54 @@ public class Main {
         List<Observation> trainSet = csvFIleLoader.loadData(trainFile);
         List<Observation> testSet = csvFIleLoader.loadData(testFile);
 
-        double[] weights = { 0.5 , 0.5 , 0.5 , 0.5};
+        int attributes = trainSet.getFirst().getData().length;
+
+        double[] weights = new double[attributes];
+        Arrays.fill(weights, 0.5);
+
         Perceptron perceptron = new Perceptron(weights,0);
 
         Trainer trainer =  new Trainer(trainSet, a, perceptron);
         trainer.learn();
         trainer.testing(testSet);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+
+            System.out.println("Please input data or type exit to exit");
+
+            String line = scanner.nextLine();
+
+            if(line.equals("exit")){
+                break;
+            }
+
+            String[] parts = line.split(",");
+            double[] data = new double[parts.length];
+
+            if(parts.length != attributes){
+                System.out.println("Wrong number of data");
+                continue;
+            }
+
+            try{
+                for(int i = 0; i < parts.length; i++){
+                    data[i] = Double.parseDouble(parts[i]);
+                }
+            }catch(NumberFormatException e){
+                System.out.println("Wrong type of data");
+                continue;
+            }
+
+            int result = perceptron.Output(data);
+
+            if(result == 0){
+                System.out.println("Versicolor");
+            }else {
+                System.out.println("Virginica");
+            }
+
+        }
     }
 }
